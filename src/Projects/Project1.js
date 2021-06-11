5135083bfa3e466d61e29fe776a576a1cb097533
Thumma Bhavana <bhavanathumma3@msitprogram.net>
2021-06-11 21:39:37 +0530

    Project pages

--- src/Projects/Project1.js
+++ src/Projects/Project1.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Header from '../Sections/Header';
+import Footer from '../Sections/Footer';
+// import { Carousel, Row, Col } from 'react-bootstrap';
 
 /**
 * @author
@@ -6,31 +9,73 @@ import React from 'react'
 **/
 
 const Project1 = (props) => {
-  return(
+  return (
     <div>
+      {/* <Row>
+        <Col sm={4}>
+          <Carousel>
+            <Carousel.Item interval={1000}>
+              <img
+                className="d-block w-100"
+                src="try.PNG"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={500}>
+              <img
+                className="d-block w-100"
+                src="try.PNG"
+                alt="Second slide"
+              />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="work2.jpg"
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+      </Row> */}
+
         <h4>Technologies used: </h4>
         <h6>
-        Frontend : Html, CSS, Bootstrap, React JS<br/>
-        Code Versioning Systems: Git and GitHub</h6><br/>
+          Frontend : Html, CSS, Bootstrap, React JS<br/>
+          Code Versioning Systems: Git and GitHub</h6><br/>
         <p>
-        <strong>Sprint 1: Learnings</strong><br/>
-        Worked on product backlog to understand. Work on Sign-in page appearance where user email and password have to be verified to match the requirement and eye symbol has to be included for password visibility. For sign-in also included social login buttons for a better user experience. Worked on setting up the navbar for sign-in and signup components with toggle mode. Setting up the font size, family, etc, took little time. We have taken the feedback from mentors and peers to verify our sprint work and noted changes that are needed to be made. The colors and animation settings are done. Sign-in and Signup buttons were set. Integrated all the pages and worked on their responsiveness. Finally, an appealing sign-in signup page is created.
-        </p><br/>
-        <p>
-        <strong>Sprint 2: Learnings</strong><br/>
-        Integrated all the pages and make them responsive. Learned basics of AWS Amplify and Cognito and practiced authentication. Analyzed the cost of AWS Amplify and Cognito. Prepared the document on cost prediction, learned the workflow with Amplify to authenticate, and started working on sign-up and sign-in authentication. Working on sign-up and sign-in authentication.
-        </p><br/>
-        <p>
-        <strong>Sprint 3: Learnings</strong><br/>
-        Learn How to get skills from the LinkedIn profile with the help of documentation. Worked on aspiration page UI. Worked on the basic design of the Aspiration page UI, which has one autocomplete field, a drop-down, a navbar with page links and image showing statistics, and a button to proceed. tried for a better look of autocomplete field use the scroll bar for the data list to fit into the area. set the page and update the job titles with the given JSON file. later populate the options with titles got from API call.
-        </p><br/>
-        <p>
-        <strong>Sprint 4: Learnings</strong><br/>
-        Getting job titles from JSON file is redundant. Worked on finding libraries for text extraction from any type of file. Acquired exposure on getting a specific text like email, phone number, skills, etc using NLP. Could read the pdf and txt files. Worked to find different resources to get skills from the resume for further process. Able to extract skills from the pdf and text file given the list of skills (Bruteforce Algorithm). Worked on UI theme and color to make it bluer for a better look.
-        </p><br/>
-    </div>
-   )
 
- }
+          <strong>Sprint 1: Learnings</strong><br/>
+          Worked on product backlog to understand. Work on Sign-in page appearance where user email and password have to be verified to match the requirement and eye symbol has to be included for password visibility. For sign-in also included social login buttons for a better user experience. Worked on setting up the navbar for sign-in and signup components with toggle mode. Setting up the font size, family, etc, took little time. We have taken the feedback from mentors and peers to verify our sprint work and noted changes that are needed to be made. The colors and animation settings are done. Sign-in and Signup buttons were set. Integrated all the pages and worked on their responsiveness. Finally, an appealing sign-in signup page is created.
+        </p><br />
+        <p>
+          <strong>Sprint 2: Learnings</strong><br/>
+          Integrated all the pages and make them responsive. Learned basics of AWS Amplify and Cognito and practiced authentication. Analyzed the cost of AWS Amplify and Cognito. Prepared the document on cost prediction, learned the workflow with Amplify to authenticate, and started working on sign-up and sign-in authentication. Working on sign-up and sign-in authentication.
+        </p><br />
+        <p>
+          <strong>Sprint 3: Learnings</strong><br/>
+          Learn How to get skills from the LinkedIn profile with the help of documentation. Worked on aspiration page UI. Worked on the basic design of the Aspiration page UI, which has one autocomplete field, a drop-down, a navbar with page links and image showing statistics, and a button to proceed. tried for a better look of autocomplete field use the scroll bar for the data list to fit into the area. set the page and update the job titles with the given JSON file. later populate the options with titles got from API call.
+        </p><br />
+        <p>
+          <strong>Sprint 4: Learnings</strong><br/>
+          Getting job titles from JSON file is redundant. Worked on finding libraries for text extraction from any type of file. Acquired exposure on getting a specific text like email, phone number, skills, etc using NLP. Could read the pdf and txt files. Worked to find different resources to get skills from the resume for further process. Able to extract skills from the pdf and text file given the list of skills (Bruteforce Algorithm). Worked on UI theme and color to make it bluer for a better look.
+        </p><br />
+
+    </div>
+  )
+
+}
 
 export default Project1
